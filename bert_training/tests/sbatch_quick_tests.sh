1ca425cf9f5f77afd7e62e2fde6b4bc5aaf9906f
Harry-Anton Talvik <harry-anton.talvik@stacc.ee>
2021-08-23 17:17:38 +0300

    #1573 - Script to run quick tests with browsing output on UTHPC's rocket

--- bert_training/tests/sbatch_quick_tests.sh
+++ bert_training/tests/sbatch_quick_tests.sh
@@ -7,18 +7,32 @@
 #SBATCH --mem=500M
 #SBATCH -t 00:10:00
 
+# Submit this job so:
+#   sbatch sbatch_quick_tests.sh
+# and then browse the output:
+#   cat slurm-{job-id}.out
+
+echo "START: sbatch_quick_tests.sh"
+
+echo "Load module 'miniconda3/4.8.2'..."
 module load miniconda3/4.8.2
+echo "Module loading done."
 
 # MAGIC to avoid
 #   CommandNotFoundError: Your shell has not been properly configured to use 'conda activate'.
+echo "Prepare for conda env activation ..."
 echo $'Calling: eval "$(conda shell.bash hook)"'
 eval "$(conda shell.bash hook)"
+echo "Done."
 
+echo "Activate conda environment 'medbert'..."
 conda activate medbert
+echo "Done, env activated."
 
 # Slow tests are skipped by default, others can be run like so:
 # (medbert) $ python -m unittest discover -vvv
+echo "Discover and run all (non-slow) tests..."
 python -m unittest discover -vvv
+echo "Done, all discovered tests were run."
 
-# Submit this job so:
-#   sbatch sbatch_quick_tests.sh
+echo "END: sbatch_quick_tests.sh"
