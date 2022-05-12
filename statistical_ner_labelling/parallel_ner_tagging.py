28bf3c412519cc310023fb18f928f0db17704511
Rasmus Maide <rasmusmaide@gmail.com>
2022-05-12 13:57:29 +0300

    pipeline improvements

--- statistical_ner_labelling/parallel_ner_tagging.py
+++ statistical_ner_labelling/parallel_ner_tagging.py
@@ -4,11 +4,14 @@ import multiprocessing
 from estnltk.storage import PostgresStorage
 from estnltk.taggers import NerTagger
 
+import sys
+import os
+
 
 class ParallelNerTagger:
 
     def __init__(self, dbname, user, password, host, port, role, threads, collection, morph_layer, words_layer,
-                 sentences_layer, output_layer):
+                 sentences_layer, output_layer, schema):
         self.dbname = dbname
         self.user = user
         self.password = password
@@ -21,25 +24,39 @@ class ParallelNerTagger:
         self.words_layer = words_layer
         self.sentences_layer = sentences_layer
         self.output_layer = output_layer
+        self.schema = schema
 
     def ner_thread(self, block):
         storage = PostgresStorage(dbname=self.dbname, user=self.user, password=self.password, host=self.host,
-                                  port=self.port, role=self.role)
+                                  port=self.port, role=self.role, schema=self.schema)
         collection = storage.get_collection(self.collection)
         morph_layer = self.morph_layer
         words_layer = self.words_layer
         sentences_layer = self.sentences_layer
-        output_layer = self.output_layer
         collection.selected_layers = [morph_layer, words_layer, sentences_layer]
-        ner_tagger = NerTagger(output_layer=output_layer, morph_layer_input=morph_layer, words_layer_input=words_layer,
+        ner_tagger = NerTagger(output_layer=self.output_layer, morph_layer_input=morph_layer,
+                               words_layer_input=words_layer,
                                sentences_layer_input=sentences_layer)
-        # change the number based on the number of parallel threads and also in the driver_func
+
         collection.create_layer_block(tagger=ner_tagger, block=(self.threads, block))
+        storage.close()
 
     def driver_func(self):
-        # change the number based on the number of parallel threads and also in the ner_thread
         processes = self.threads
         procs = []
+        morph_layer = self.morph_layer
+        words_layer = self.words_layer
+        sentences_layer = self.sentences_layer
+        output_layer = self.output_layer
+        storage = PostgresStorage(dbname=self.dbname, user=self.user, password=self.password, host=self.host,
+                                  port=self.port, role=self.role, schema=self.schema)
+        collection = storage.get_collection(self.collection)
+        ner_tagger = NerTagger(output_layer=output_layer, morph_layer_input=morph_layer,
+                               words_layer_input=words_layer,
+                               sentences_layer_input=sentences_layer)
+        collection.add_layer(layer_template=ner_tagger.get_layer_template())
+        storage.close()
+
         for i in range(processes):
             proc = multiprocessing.Process(target=self.ner_thread, args=(i,))
             procs.append(proc)
@@ -51,7 +68,40 @@ class ParallelNerTagger:
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
-    config.read('workflow_config.ini')
+    conf_file = sys.argv[1]
+    file_name = os.path.abspath(os.path.expanduser(os.path.expandvars(str(conf_file))))
+
+    if not os.path.exists(file_name):
+        raise ValueError("File {file} does not exist".format(file=str(conf_file)))
+
+    if len(config.read(file_name)) != 1:
+        raise ValueError("File {file} is not accessible or is not in valid INI format".format(file=conf_file))
+
+    if not config.has_section('database-configuration'):
+        prelude = "Error in file {}\n".format(file_name) if len(file_name) > 0 else ""
+        raise ValueError("{prelude}Missing a section [{section}]".format(prelude=prelude, section='database-configuration'))
+    for option in ["server", "port", "database", "username", "password","role","schema","collection","morph_layer",
+                   "words_layer","sentences_layer"]:
+        if not config.has_option('database-configuration', option):
+            prelude = "Error in file {}\n".format(file_name) if len(file_name) > 0 else ""
+            raise ValueError(
+                "{prelude}Missing option {option} in the section [{section}]".format(
+                    prelude=prelude, option=option, section='database-configuration'
+                )
+            )
+    if not config.has_section('ner-configuration'):
+        prelude = "Error in file {}\n".format(file_name) if len(file_name) > 0 else ""
+        raise ValueError("{prelude}Missing a section [{section}]".format(prelude=prelude, section='ner-configuration'))
+    for option in ["num_threads","output_layer"]:
+        if not config.has_option('ner-configuration', option):
+            prelude = "Error in file {}\n".format(file_name) if len(file_name) > 0 else ""
+            raise ValueError(
+                "{prelude}Missing option {option} in the section [{section}]".format(
+                    prelude=prelude, option=option, section='ner-configuration'
+                )
+            )
+
+    config.read(file_name)
 
     parallel_ner_tagger = ParallelNerTagger(dbname=config['database-configuration']['database'],
                                             user=config['database-configuration']['username'],
@@ -64,6 +114,7 @@ if __name__ == '__main__':
                                             morph_layer=config['database-configuration']['morph_layer'],
                                             words_layer=config['database-configuration']['words_layer'],
                                             sentences_layer=config['database-configuration']['sentences_layer'],
-                                            output_layer=config['ner-configuration']['output_layer'])
+                                            output_layer=config['ner-configuration']['output_layer'],
+                                            schema=config['database-configuration']['schema'])
 
     parallel_ner_tagger.driver_func()
--- statistical_ner_labelling/readme.md
+++ statistical_ner_labelling/readme.md
@@ -12,16 +12,58 @@ graph TD;
     curation.ipynb-->rule_based_ner_tagging.ipynb;
 ```
 
-* parallel_ner_tagging.py uses parallel computing on EstNLTK collections to apply the NER tagger on a large corpus.
+### parallel_ner_tagging.py
+uses parallel computing on EstNLTK collections to apply the NER tagger on a large corpus.
 Here it is applied on Koondkorpus but it can be used on other corpora as well.
-* entity_counting.py aggregates the data from the NER tagging file to CSV files
+
+```mermaid
+graph LR;
+    id1[collection without the NER layer]-->parallel_ner_tagging.py;
+    parallel_ner_tagging.py-->id2[collection with NER layer];
+```
+
+
+### entity_counting.py
+aggregates the data from the NER tagging file to CSV files
   divided by if the entity is composed of multiple tokens or one and for multiple
   tokens it separates the last tokens and all preceding tokens.
-* curation.ipynb shows how these count lists can be used to create rulesets for
+
+```mermaid
+graph LR;
+    id3[collection with NER layer]-->entity_counting.py;
+    entity_counting.py-->id4[CSV file with counts of named entities - ner_counts.csv];
+```
+
+### curation.ipynb
+shows how these count lists can be used to create rulesets for
   statistical tagging
-* sentences_from_given_first_words_list.ipynb and sentences_from_given_last_word_list.ipynb
+
+```mermaid
+graph LR;
+    id5[CSV file with counts of named entities - ner_counts.csv]-->curation.ipynb;
+    curation.ipynb-->id6[rulesets for statistical tagging - first_ruleset.pkl, last_ruleset.pkl];
+```
+
+### sentences_from_given_first_words_list.ipynb, sentences_from_given_last_word_list.ipynb
+
   create a list of EstNLTK text objects, each containing one sentence, where any
   of the given words in a list exists.
-* rule_based_ner_tagging.py uses the curated rulesets to perform statistical ner
+
+```mermaid
+graph LR;
+    id7[ruleset from a list of first words - first_ruleset.pkl ]-->sentences_from_given_first_words_list.ipynb;
+    sentences_from_given_first_words_list.ipynb-->id8[all sentences containing these words - first_word_dataset.pkl];
+    id9[ruleset from a list of last words - last_ruleset.pkl ]-->sentences_from_given_last_word_list.ipynb;
+    sentences_from_given_last_word_list.ipynb-->id10[all sentences containing these words - last_word_dataset.pkl];
+```
+### rule_based_ner_tagging.py
+uses the curated rulesets to perform statistical ner
   tagging
-* The file workflow_config.ini contains the configuration for all the files used here.
+
+```mermaid
+graph LR;
+    id11[any ruleset from curation.ipynb]-->rule_based_ner_tagging.py;
+    rule_based_ner_tagging.py-->id12[new NER layer];
+```
+### workflow_config.ini 
+contains the configuration for all the files used here.
