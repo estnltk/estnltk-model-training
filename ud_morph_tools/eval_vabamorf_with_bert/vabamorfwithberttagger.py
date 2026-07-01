3348d59229fff6791cc5ce0d5d32be2ac95a7dd7
Kaire Koljal <kaire.koljal@gmail.com>
2026-07-01 13:41:07 +0300

    VabamorfWithBertTagger update

--- ud_morph_tools/eval_vabamorf_with_bert/vabamorfwithberttagger.py
+++ ud_morph_tools/eval_vabamorf_with_bert/vabamorfwithberttagger.py
@@ -10,62 +10,81 @@ from estnltk.taggers import PostMorphAnalysisTagger
 from estnltk_neural.taggers import BertMorphTagger
 
 
+
 class VabamorfWithBertTagger(Tagger):
     """
     Tagger that creates and refines 'morph_analysis' layer with
       1. VabamorfAnalyzer
       2. PostMorphAnalysisTagger
       3. BertMorphTagger
-      and returns 'morph_analysis' layer. 
+      and returns 'morph_analysis' or user defined layer. 
       PostMorphAnalysisTagger can be disabled using use_postanalysis=False (default True)
     """
 
-    conf_param = ['use_postanalysis', 'vabamorf', 'post_morph', 'bert']
+    conf_param = ['use_postanalysis', 'vabamorf', 'post_morph', 'bert', 'output_layer', 'slang_lex', 'device']
     output_layer = 'morph_analysis'
-    output_attributes = ['normalized_text', 'lemma', 'root', 'root_tokens', 'ending', 'clitic', 'form', 'partofspeech', '_ignore']
+    output_attributes = ['normalized_text', 'lemma', 'root', 'root_tokens', 'ending', 'clitic', 'form', 'partofspeech']
     input_layers = ()
 
     def __init__(self,
                 post_morph=None,
                  vabamorf = None,
                  bert = None,
-                 use_postanalysis=True,):
+                 use_postanalysis=True,
+                 output_layer=output_layer,
+                 slang_lex=True,
+                 device='cpu', # for gpu - 'cuda'
+                ):
 
-        self.vabamorf = VabamorfAnalyzer()
-        self.post_morph = PostMorphAnalysisTagger()
-        self.bert = BertMorphTagger(output_layer='morph_analysis', disambiguate=True)
+        self.output_layer = output_layer
+        self.slang_lex = slang_lex
+        self.device = device
+        self.vabamorf = VabamorfAnalyzer(output_layer=self.output_layer)#, slang_lex=self.slang_lex)
+        self.post_morph = PostMorphAnalysisTagger(output_layer=self.output_layer)
+        self.bert = BertMorphTagger(output_layer=self.output_layer, disambiguate=True, device=self.device)
         self.use_postanalysis = use_postanalysis
 
+       
+    def _make_layer_template(self):
+        return Layer(name=self.output_layer, attributes=self.output_attributes)
         
-    def _make_layer(self, text, layers, status=None):
-        """
-        For Tagger implementation. Calls 'tag' function.
-        """
-        return self.tag(text, layers=layers)
     
+    def _make_layer(self, text, layers, status=None):
 
-    def tag(self, text, layers=None):
-        """
-        Runs all the taggers, refines 'morph_analysis' layer and returns it.
-        """
-
+        # --------------------------------------------
+        #   Morphological analysis
+        # --------------------------------------------
         text.tag_layer( self.vabamorf.input_layers )
-        # Vabamorf and initial morph_analysis layer
-        self.vabamorf.tag(text)
+        morph_layer = self.vabamorf.make_layer( text, layers, status )
 
-        # Post-processing morph_analysis
-        if self.use_postanalysis:
-            self.post_morph.retag(text)
-
+        # --------------------------------------------
+        #   Adding necessary layers for post processing
+        # -------------------------------------------- 
+        layers2 = layers.copy()
+        layers2["words"] = text["words"]
+        layers2["sentences"] = text["sentences"]
+        layers2["compound_tokens"] = text["compound_tokens"]
+        layers2["tokens"] = text["tokens"]
+        layers2[self.output_layer] = morph_layer
+        
+        # --------------------------------------------
+        #   Post-processing after analysis
+        # --------------------------------------------
+        if self.use_postanalysis and self.post_morph:
+            self.post_morph.change_layer( text, layers2, status )
             #  Remove _ignore from the output layer
-            if IGNORE_ATTR in text["morph_analysis"].attributes:
-                text["morph_analysis"].attributes = [attr for attr in text["morph_analysis"].attributes if attr != IGNORE_ATTR]
-                for span in text["morph_analysis"]:
+            if IGNORE_ATTR in layers2[self.output_layer].attributes:
+                layers2[self.output_layer].attributes = [attr for attr in layers2[self.output_layer].attributes if attr != IGNORE_ATTR]
+                for span in layers2[self.output_layer]:
                     for annotation in span.annotations:
                         delattr(annotation, IGNORE_ATTR)
+                        
+        # --------------------------------------------
+        #   Bert based analysis
+        # --------------------------------------------
+        self.bert.change_layer( text, layers2, status )
 
-        # BERT-based corrections
-        self.bert.retag(text)
+        return morph_layer
 
-        return text["morph_analysis"]
+
 
